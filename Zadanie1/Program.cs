835516ef2f2008155dfd9451a549f2099374fcb6
VictorK2018 <victor.kalinsk@gmail.com>
2019-05-25 21:45:08 -0400

    Задание1

---
+++ Zadanie1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "https://www.google.ca/";
+
+            // WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Quit();
+        }
+    }
+}
